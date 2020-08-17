e35c68ef5c96e11501f977e556b8a72d06a679aa
Angela Williams <lambda_school_loaner_97@Lambda-School-Loaner-97s-MacBook-Pro.local>
2020-08-16 18:57:13 -0700

    Completed MVP

--- hashtables/ex4/ex4.py
+++ hashtables/ex4/ex4.py
@@ -3,8 +3,21 @@ def has_negatives(a):
     YOUR CODE HERE
     """
     # Your code here
+    # creating dictionary
+    dict_t = {}
+    # if positive and negative, will append
+    both = []
+    # walking through given values
+    for i in a:
+        # abs function to flip positive to neg
+        i = abs(i)  
+        # if number is not in dictionary, create an index and assign value
+        if i not in dict_t:
+            dict_t[i] = 1
+        else:
+            both.append(i)
 
-    return result
+    return both
 
 
 if __name__ == "__main__":
