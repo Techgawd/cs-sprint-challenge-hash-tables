953fdd8767d4bd44ce2df9de73854e06ed1e765d
Angela Williams <lambda_school_loaner_97@Lambda-School-Loaner-97s-MacBook-Pro.local>
2020-08-16 17:27:43 -0700

    Completed ex1

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -3,5 +3,15 @@ def get_indices_of_item_weights(weights, length, limit):
     YOUR CODE HERE
     """
     # Your code here
+    # create dictionary
+    items = {}
+    # walk through each index and the value for each index
+    for i, weight in enumerate(weights):
+        # retrieve target value, limit minus amount
+        weight_index = limit - weights[i]
+        # populate the dictionary if target not found
+        if weight_index not in items:
+            items[weight] = i
+        else:
+            return (i, items[weight_index])
 
-    return None
